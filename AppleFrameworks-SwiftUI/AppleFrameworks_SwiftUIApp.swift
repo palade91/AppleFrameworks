d73c5b9c3b6475a7de3f1d5fe112783349f607d1
Catalin Palade <catalin@recruitd.com>
2020-12-22 15:29:46 +0000

    Initial Commit

---
+++ AppleFrameworks-SwiftUI/AppleFrameworks_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworks_SwiftUIApp.swift
+//  AppleFrameworks-SwiftUI
+//
+//  Created by Catalin Palade on 22/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworks_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
