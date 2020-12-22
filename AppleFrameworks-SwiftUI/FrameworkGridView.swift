6f2acb06f6ac047488a557464ff40f69ee6127ba
Catalin Palade <catalin@recruitd.com>
2020-12-22 15:41:28 +0000

    initial project setup, assets + mockup data

---
+++ AppleFrameworks-SwiftUI/FrameworkGridView.swift
@@ -0,0 +1,20 @@
+//
+//  FrameworkGridView.swift
+//  AppleFrameworks-SwiftUI
+//
+//  Created by Catalin Palade on 22/12/2020.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FrameworkGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkGridView()
+    }
+}
