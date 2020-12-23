425d09e4e1c5025537dbbabb1348846415bed392
Catalin Palade <catalin@recruitd.com>
2020-12-23 13:13:51 +0000

    added DetailView

---
+++ AppleFrameworks-SwiftUI/AFButton.swift
@@ -0,0 +1,29 @@
+//
+//  AFButton.swift
+//  AppleFrameworks-SwiftUI
+//
+//  Created by Catalin Palade on 23/12/2020.
+//
+
+import SwiftUI
+
+struct AFButton: View {
+    
+    var title: String
+    
+    var body: some View {
+        Text(title)
+            .font(.title2)
+            .fontWeight(.semibold)
+            .frame(width: 280, height: 50)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+    }
+}
+
+struct AFButton_Previews: PreviewProvider {
+    static var previews: some View {
+        AFButton(title: "Test Title")
+    }
+}
---
+++ AppleFrameworks-SwiftUI/FrameworkDetailView.swift
@@ -0,0 +1,53 @@
+//
+//  FrameworkDetailView.swift
+//  AppleFrameworks-SwiftUI
+//
+//  Created by Catalin Palade on 23/12/2020.
+//
+
+import SwiftUI
+
+struct FrameworkDetailView: View {
+    
+    let framework: Framework
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "xmark")
+                        .foregroundColor(Color(.label))
+                        .imageScale(.large)
+                        .frame(width: 44, height: 44)
+                }
+            }
+            .padding()
+            
+            Spacer()
+            
+            FrameworkTitleView(framework: framework)
+            
+            Text(framework.description)
+                .font(.body)
+                .padding()
+            
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                AFButton(title: "Learn More")
+            }
+        }
+    }
+}
+
+struct FrameworkDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkDetailView(framework: MockData.sampleFramework)
+    }
+}
