a1ab077576648ac2e8dd2c4466373a0772c36462
Catalin Palade <catalin@recruitd.com>
2020-12-23 14:56:09 +0000

    Refactoring & Cleaning + Project organizatiom

--- AppleFrameworks-SwiftUI/Screens/FrameworkGridView/FrameworkGridView.swift
+++ AppleFrameworks-SwiftUI/Screens/FrameworkGridView/FrameworkGridView.swift
@@ -11,13 +11,11 @@ struct FrameworkGridView: View {
     
     @StateObject var viewModel = FrameworkGridViewModel()
     
-    let columns: [GridItem] = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
-    
     var body: some View {
         
         NavigationView {
             ScrollView {
-                LazyVGrid(columns: columns, content: {
+                LazyVGrid(columns: viewModel.columns, content: {
                     ForEach(MockData.frameworks) { framework in
                         FrameworkTitleView(framework: framework)
                             .onTapGesture {
@@ -38,25 +36,5 @@ struct FrameworkGridView: View {
 struct FrameworkGridView_Previews: PreviewProvider {
     static var previews: some View {
         FrameworkGridView()
-            .preferredColorScheme(.dark)
-    }
-}
-
-struct FrameworkTitleView: View {
-    
-    let framework: Framework
-    
-    var body: some View {
-        VStack {
-            Image(framework.imageName)
-                .resizable()
-                .frame(width: 90, height: 90)
-            Text(framework.name)
-                .font(.title2)
-                .fontWeight(.semibold)
-                .scaledToFit()
-                .minimumScaleFactor(0.6)
-        }
-        .padding()
     }
 }
---
+++ AppleFrameworks-SwiftUI/Views/Buttons/XDismissButton.swift
@@ -0,0 +1,35 @@
+//
+//  XDismissButton.swift
+//  AppleFrameworks-SwiftUI
+//
+//  Created by Catalin Palade on 23/12/2020.
+//
+
+import SwiftUI
+
+struct XDismissButton: View {
+    
+    @Binding var isShowingDetailView: Bool
+    
+    var body: some View {
+        HStack {
+            Spacer()
+            
+            Button {
+                isShowingDetailView = false
+            } label: {
+                Image(systemName: "xmark")
+                    .foregroundColor(Color(.label))
+                    .imageScale(.large)
+                    .frame(width: 44, height: 44)
+            }
+        }
+        .padding()
+    }
+}
+
+struct XDismissButton_Previews: PreviewProvider {
+    static var previews: some View {
+        XDismissButton(isShowingDetailView: .constant(false))
+    }
+}
