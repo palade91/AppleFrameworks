2cd1bdf19ac2e0a695538a0e454bb49e24356b68
Catalin Palade <catalin@recruitd.com>
2020-12-23 12:56:41 +0000

    LazyVGrid with dynamic data

--- AppleFrameworks-SwiftUI/FrameworkGridView.swift
+++ AppleFrameworks-SwiftUI/FrameworkGridView.swift
@@ -8,13 +8,46 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns, content: {
+                    ForEach(MockData.frameworks) { framework in
+                        FrameworkTitleView(framework: framework)
+                    }
+                })
+            }
+            .navigationTitle("🍏 Frameworks")
+        }
     }
 }
 
 struct FrameworkGridView_Previews: PreviewProvider {
     static var previews: some View {
         FrameworkGridView()
+            .preferredColorScheme(.dark)
+    }
+}
+
+struct FrameworkTitleView: View {
+    
+    let framework: Framework
+    
+    var body: some View {
+        VStack {
+            Image(framework.imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            Text(framework.name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
+        .padding()
     }
 }
